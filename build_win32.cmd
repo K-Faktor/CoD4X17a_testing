139b2f3865d1229297362fbc7587772525d64835
TheKelm <TheKelm@users.noreply.github.com>
2014-03-15 01:21:40 +0100

    Hotfix

--- build_win32.cmd
+++ build_win32.cmd
@@ -11,17 +11,17 @@ gcc -m32 -Wall -O0 -g -fno-omit-frame-pointer -mtune=prescott -D WINVER=0x501 -I
 cd ../
 
 echo Compiling NASM...
-nasm -f coff qcommon_hooks.asm         --prefix _ -o bin/qcommon_hooks.o
-nasm -f coff cmd_hooks.asm             --prefix _ -o bin/cmd_hooks.o
-nasm -f coff filesystem_hooks.asm      --prefix _ -o bin/filesystem_hooks.o
-nasm -f coff xassets_hooks.asm         --prefix _ -o bin/xassets_hooks.o
-nasm -f coff trace_hooks.asm           --prefix _ -o bin/trace_hooks.o
-nasm -f coff misc_hooks.asm            --prefix _ -o bin/misc_hooks.o
-nasm -f coff scr_vm_hooks.asm          --prefix _ -o bin/scr_vm_hooks.o
-nasm -f coff g_sv_hooks.asm            --prefix _ -o bin/g_sv_hooks.o
-nasm -f coff server_hooks.asm          --prefix _ -o bin/server_hooks.o
-nasm -f coff msg_hooks.asm             --prefix _ -o bin/msg_hooks.o
-nasm -f coff pluginexports.asm -dWin32 --prefix _ -o bin/pluginexports.o
+nasm -f coff src/qcommon_hooks.asm         --prefix _ -o bin/qcommon_hooks.o
+nasm -f coff src/cmd_hooks.asm             --prefix _ -o bin/cmd_hooks.o
+nasm -f coff src/filesystem_hooks.asm      --prefix _ -o bin/filesystem_hooks.o
+nasm -f coff src/xassets_hooks.asm         --prefix _ -o bin/xassets_hooks.o
+nasm -f coff src/trace_hooks.asm           --prefix _ -o bin/trace_hooks.o
+nasm -f coff src/misc_hooks.asm            --prefix _ -o bin/misc_hooks.o
+nasm -f coff src/scr_vm_hooks.asm          --prefix _ -o bin/scr_vm_hooks.o
+nasm -f coff src/g_sv_hooks.asm            --prefix _ -o bin/g_sv_hooks.o
+nasm -f coff src/server_hooks.asm          --prefix _ -o bin/server_hooks.o
+nasm -f coff src/msg_hooks.asm             --prefix _ -o bin/msg_hooks.o
+nasm -f coff src/pluginexports.asm -dWin32 --prefix _ -o bin/pluginexports.o
 
 echo Linking...
 gcc -g -o bin/cod4x17a_dedrun bin/*.o win32/win_cod4.res -Llib/ -ltomcrypt_win32 -ltommath_win32 -lm -lws2_32 -lwsock32 -lgdi32 -mwindows -lwinmm
